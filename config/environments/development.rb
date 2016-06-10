e1e299cc838db314e2e5f12d2a678dea97d0d9a0
ashr81 <ashrithreddi81@gmail.com>
2016-06-11 00:44:02 +0530

    Removed MailSending code from development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,16 +26,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
