7a7fa97d3efe6edfcdef494ef81b4241d03ffdfa
ashr81 <ashrithreddi81@gmail.com>
2016-06-11 01:07:39 +0530

    changed environment variables

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'ashr81',
+  :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
