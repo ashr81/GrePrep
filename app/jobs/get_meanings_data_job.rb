95bde68aab184233ebc82445ecb7b733cf061167
ashr81 <ashrithreddi81@gmail.com>
2016-06-16 00:49:20 +0530

    Adds deliver to send the email

--- app/jobs/get_meanings_data_job.rb
+++ app/jobs/get_meanings_data_job.rb
@@ -4,7 +4,7 @@ class GetMeaningsDataJob < ApplicationJob
   def self.perform(user_id)
     data = GetMeaningsData.get_words_total_data(User.find(user_id).meanings)
     Rails.logger.debug "inside GetMeaningsDataJob after getting data"
-    UserMailer.meanings_data(data)
+    UserMailer.meanings_data(data).deliver
     Rails.logger.debug "inside GetMeaningsDataJob after sending email"
   end
 end
