c6a41a2523ff1aca3029dfaa2511c410b0bea19e
ashr81 <ashrithreddi81@gmail.com>
2016-06-16 00:30:50 +0530

    Add Self. to GetMeaningsDataJob

--- app/jobs/get_meanings_data_job.rb
+++ app/jobs/get_meanings_data_job.rb
@@ -1,7 +1,7 @@
 class GetMeaningsDataJob < ApplicationJob
   queue_as :default
 
-  def perform(user_id)
+  def self.perform(user_id)
     data = GetMeaningsData.get_words_total_data(User.find(user_id).meanings)
     UserMailer.meanings_data(data)
   end
